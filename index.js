133ec448efe4ca367c06e17f18091f7d2a692d89
kazuya kawaguchi <kawakazu80@gmail.com>
2017-04-03 02:29:35 +0900

    :up: update: logging

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ module.exports = {
   hooks: {
     "page:before": function (page) {
       console.log('page.title', page.title)
-      console.log('page.content', page.content)
       console.log('page.path', page.path)
       console.log('page.rawPath', page.rawPath)
       console.log('page.type', page.type)
+      console.log('this.book.language', this.book.language)
     }
   },
 
